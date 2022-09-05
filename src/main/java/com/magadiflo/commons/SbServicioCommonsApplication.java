982ca2c991a1b78f6e5764a1fa155025b9c784fe
Martín <magadiflo@gmail.com>
2022-09-05 17:19:30 -0500

    Modificando servicio productos para agregar dependencia commons

--- src/main/java/com/magadiflo/commons/SbServicioCommonsApplication.java
+++ src/main/java/com/magadiflo/commons/SbServicioCommonsApplication.java
@@ -1,8 +1,11 @@
 package com.magadiflo.commons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) //Deshabilitamos la Autoconfiguración del DataSource
 public class SbServicioCommonsApplication {
 	
 
